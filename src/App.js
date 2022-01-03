58c9167c19357e19813d059ae0c3a261231a42e6
Just-Doo <engus200@naver.com>
2022-01-04 01:35:05 +0900

    git push -u origin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	let [title, titleChanger] = useState(['힙합', '발라드', 'UK팝', '펑크락']);
+
+	let [like, likeChanger] = useState([0, 0, 0, 0]);
+		
+	function increaseLike1(){
+		let newlike = [...like];
+		newlike[0] = newlike[0]+1
+		likeChanger( 
+			newlike
+		);
+
+		console.log(`like1 :` +newlike);
+	}
+
+	function increaseLike2(){
+		let newlike = [...like];
+		newlike[1] = newlike[1]+1
+		likeChanger( 
+			newlike
+		);
+
+		console.log(`like1 :` +newlike);
+	}
+
+	function increaseLike3(){
+		let newlike = [...like];
+		newlike[2] = newlike[2]+1
+		likeChanger( 
+			newlike
+		);
+
+		console.log(`like1 :` +newlike);
+	}
+
+	function increaseLike4(){
+		let newlike = [...like];
+		newlike[3] = newlike[3]+1
+		likeChanger( 
+			newlike
+		);
+
+		console.log(`like1 :` +newlike);
+	}
+
+	let likeIncreaser = [increaseLike1, increaseLike2, increaseLike3, increaseLike4];
+
+	function list(){
+		let i = 0;
+		let len = title.length;
+		let a = [];
+
+		for ( i = 0; i < len; i++ ) {
+			a.push
+			(
+				<div className='list'>
+					<h2>
+						{ title[i] } 
+						<span onClick={ 
+							likeIncreaser[i]
+						 }>
+							like
+						</span> {like[i]};
+					</h2>
+					<p> 1월 2일 발행 </p>
+					<hr/>
+				</div>
+			)
+		}
+		return a;
+	}
+
+	function changeTitle(){
+		let newArr = [...title];
+		newArr[0] = newArr[0] +1
+		titleChanger( newArr );
+		console.log(newArr);
+	}
+	
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<div style={ {color : 'blue', fontSize : '30px'} }>개발 BLog</div>
+			</div>
+			
+			<button onClick={ changeTitle }>chagner title</button>
+
+			
+			{ list() }
+
+			<div className='modal'>
+				<h2>
+					title
+				</h2>
+				<p>
+					date
+				</p>
+				<p>
+					content
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
